d1011de160bfb62bfe57321d390b5161dde8e6d4
Amir Shaqed <amshaqed@gmail.com>
2026-09-14 16:58:31 +0300

    Committing changes to make the server a functional class and a handler class to handle threads. I now know this is in a later request...

--- server.py
+++ server.py
@@ -2,25 +2,50 @@ import argparse
 import socket
 import struct
 import sys
+import threading
 
-def run_server(host, port):
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind((host, port))
-        s.listen()
-        print(f'Server listening on {host}:{port}')
-        while True:
-            conn, addr = s.accept()
-            with conn:
-                print(f'Connected by {addr}')
-                data_header = conn.recv(4)
-                if not data_header:
-                    break
-                data_length = struct.unpack('<i', data_header)[0]
-                data = conn.recv(data_length).decode()
-                print(f'Received data: {data}')
-                response = f"Data received: {data}"
-                conn.sendall(response.encode())
+class Server:
+    def __init__(self,host:str,port:int)->Server:
+        self.host=host
+        self.port=port
+
+    def start(self)->None:
+        listener=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        listener.bind((self.host,self.port))
+        listener.listen(1000)
+        print(f'Server listening on {self.host}:{self.port}')
+        try:
+            while True:
+                conn, addr = listener.accept()
+                handle=Handler(conn)
+                handle.start()
+        except KeyboardInterrupt:
+            print("\nExiting server as per user request!")
+            conn.close()
+
                 
+
+#We are inspired by the slides!
+class Handler(threading.Thread):
+    def __init__(self, connection:socket.socket):
+        super().__init__()
+        self.connection=connection
+        
+    def run(self):
+        try:
+            con = self.connection
+            data_header = con.recv(4)
+            if not data_header:
+                return
+            data_length = struct.unpack('<i', data_header)[0]
+            data = con.recv(data_length).decode()
+            print(f'Received data: {data}\nData length: {data_length}')
+            response = f"Data received: {data}"
+            con.sendall(response.encode())
+        except ConnectionError:
+            print("Connection Lost!")
+
+
 def get_args():
     parser = argparse.ArgumentParser(description='Send data to server.')
     parser.add_argument('server_ip', type=str,
@@ -32,12 +57,12 @@ def get_args():
 
 def main():
     '''
-    Implementation of CLI and sending data to server.
+    Implementation of a server.
     '''
     args = get_args()
     try:
-        run_server(args.server_ip, args.server_port)
-        print('Done.')
+        s = Server(args.server_ip, args.server_port)
+        s.start()
     except Exception as error:
         print(f'ERROR: {error}')
         return 1
